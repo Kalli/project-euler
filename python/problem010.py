10da1a0583671b134db8d4639fd107f060aa993b
Karl Tryggvason <ktryggvason@gmail.com>
2015-04-28 21:57:55 +0100

    Updating problem 10 solution, using the Sieve of Eratosthenes

--- python/problem010.py
+++ python/problem010.py
@@ -6,27 +6,16 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 '''
-import math
 
 
-def is_prime(n):
-    prime = n != 1
-    i = 2
-    while i <= math.sqrt(n):
-        if n % i == 0:
-            prime = False
-            break
-        i += 1
-    return prime
+def prime_sieve_sum(n):
+    l = [0, 0] + range(2, n+1)
+    for i in l:
+        if i != 0:
+            p = 2*i
+            while p < n:
+                l[p] = 0
+                p += i
+    return sum(l)
 
-
-def prime_sum(n):
-    s, i = 0, 0
-    while i < n:
-        if is_prime(i):
-            s += i
-        i += 1
-    print s
-    return s
-
-print prime_sum(2000000)
+print prime_sieve_sum(2000000)
